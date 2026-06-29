55d35c6353b027dfd046a793defad7505fb9c1e6
YerovaArusu <Yerova.Kalvin@gmail.com>
2026-06-29 21:18:08 +0200

    fixes to make the android app run

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_26
+        targetCompatibility = JavaVersion.VERSION_26
     }
 }
--- app/androidApp/src/main/kotlin/at/yerova/arsascend/MainActivity.kt
+++ app/androidApp/src/main/kotlin/at/yerova/arsascend/MainActivity.kt
@@ -1,5 +1,6 @@
 package at.yerova.arsascend
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.Window
 import androidx.activity.ComponentActivity
@@ -7,10 +8,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
         super.onCreate(savedInstanceState)
 
         setContent {
